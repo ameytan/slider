b61bbe86a3d803954b575a1acbb9d84901cc75b4
Admin <admin.push@hackerrank.workspace>
2022-11-27 12:33:34 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import 'h8k-components'
 
 import { image1, image2, image3, image4 } from './assets/images'
@@ -26,10 +26,47 @@ function App() {
     }
   ]
 
-  const [ catalogs ] = useState([...catalogsList])
+  const [ catalogs, setCatalogs ] = useState([...catalogsList])
   const [ activeIndex, setActiveIndex ] = useState(0)
+  const [ isSliding, setIsSliding ] = useState(false)
   const [ slideTimer, setSlideTimer ] = useState(null)
   const [ slideDuration ] = useState(3000)
+
+  const handleNextClick = () => {
+    if (activeIndex == 3) {
+      const _catalogs = catalogs.slice(1).concat(catalogs[0]);
+      setCatalogs([..._catalogs]);
+    } else {
+      setActiveIndex(activeIndex + 1);
+    }
+  }
+
+  const handlePrevClick = () => {
+    if (activeIndex == 0) {
+      const _catalogs = new Array(catalogs[3]).concat(catalogs.slice(0, 3));
+      setCatalogs([..._catalogs]);
+    } else {
+      setActiveIndex(activeIndex - 1);
+    }
+  }
+
+  const handleThumbClick = (idx) => {
+    setActiveIndex(idx);
+  }
+
+  const handleSliding = (e) => {
+    const _val = e.target.checked;
+    setIsSliding(_val);
+  }
+
+  useEffect(() => {
+    if (isSliding) {
+      const interval = setInterval(() => {
+        handleNextClick();
+      }, slideDuration);
+      return () => clearInterval(interval);    
+    }
+  }, [isSliding, activeIndex, catalogs]);
 
   return (
     <Fragment>
@@ -42,16 +79,19 @@ function App() {
             <button 
               className="icon-only outlined"
               data-testid="prev-slide-btn"
+              onClick={handlePrevClick}
             >
               <i className="material-icons">arrow_back</i>
             </button>
               <Thumbs 
                 items={ catalogs } 
                 currentIndex={ activeIndex } 
+                handleClick={handleThumbClick}
               />
             <button 
               className="icon-only outlined"
               data-testid="next-slide-btn"
+              onClick={handleNextClick}
             >
               <i className="material-icons">arrow_forward</i>
             </button>
@@ -62,6 +102,7 @@ function App() {
           <input 
             type='checkbox'
             data-testid='toggle-slide-show-button'
+            onChange={handleSliding}
           /> 
           <label className='ml-6'>Start Slide Show</label>
         </div>
@@ -71,4 +112,3 @@ function App() {
 }
 
 export default App
-
